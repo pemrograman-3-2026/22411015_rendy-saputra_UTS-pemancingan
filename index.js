c76952359adc2735c7c4670160e817f84b11cc21
Rendysaputr <z76602256@gmail.com>
2026-07-05 13:36:28 +0700

    url uts

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import path from 'path'
 const app = express()
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:3000',
   credentials: true,
 }))
 const imagepath = express.static(path.join(process.cwd(), 'uploads'))
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
 app.use('/user', UserRoute)
 app.use('/pesanan', PesananRoute)
 app.use('/produk', ProdukRoute)
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(3100, () => {
+  console.log('Server is running on http://localhost:3100')
 })
